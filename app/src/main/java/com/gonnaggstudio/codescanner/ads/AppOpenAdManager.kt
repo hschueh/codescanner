5a20cd331f98470118dce076872cc560aec0c2df
hschueh <chueh044@gmail.com>
2023-05-23 01:44:24 +0800

    refactor: Move AppOpenAdManager to other package and fix warnings.

---
+++ app/src/main/java/com/gonnaggstudio/codescanner/ads/AppOpenAdManager.kt
@@ -0,0 +1,126 @@
+package com.gonnaggstudio.codescanner.ads
+
+import android.app.Activity
+import android.content.Context
+import android.util.Log
+import com.gonnaggstudio.codescanner.MainApplication
+import com.google.android.gms.ads.AdError
+import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.FullScreenContentCallback
+import com.google.android.gms.ads.LoadAdError
+import com.google.android.gms.ads.appopen.AppOpenAd
+import java.util.*
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@Singleton
+class AppOpenAdManager @Inject constructor() {
+
+    private var appOpenAd: AppOpenAd? = null
+    private var isLoadingAd = false
+    var isShowingAd = false
+
+    /** Keep track of the time an app open ad is loaded to ensure you don't show an expired ad. */
+    private var loadTime: Long = 0
+
+    /** Request an ad. */
+    fun loadAd(context: Context) {
+        // Do not load ad if there is an unused ad or one is already loading.
+        if (isLoadingAd || isAdAvailable()) {
+            return
+        }
+
+        isLoadingAd = true
+        val request = AdRequest.Builder().build()
+        AppOpenAd.load(
+            /* context = */ context,
+            /* adUnitId = */ AD_TEST_UNIT_ID,
+            /* adRequest = */ request,
+            /* loadCallback = */ object : AppOpenAd.AppOpenAdLoadCallback() {
+
+                override fun onAdLoaded(ad: AppOpenAd) {
+                    // Called when an app open ad has loaded.
+                    Log.d(LOG_TAG, "Ad was loaded.")
+                    appOpenAd = ad
+                    isLoadingAd = false
+                    loadTime = Date().time
+                }
+
+                override fun onAdFailedToLoad(loadAdError: LoadAdError) {
+                    // Called when an app open ad has failed to load.
+                    Log.d(LOG_TAG, loadAdError.message)
+                    isLoadingAd = false
+                }
+            }
+        )
+    }
+
+    private fun wasLoadTimeLessThanNHoursAgo(numHours: Long): Boolean {
+        val dateDifference: Long = Date().time - loadTime
+        val numMilliSecondsPerHour: Long = 3600000
+        return dateDifference < numMilliSecondsPerHour * numHours
+    }
+
+    /** Check if ad exists and can be shown. */
+    private fun isAdAvailable(): Boolean {
+        return appOpenAd != null && wasLoadTimeLessThanNHoursAgo(4)
+    }
+
+    /** Shows the ad if one isn't already showing. */
+    fun showAdIfAvailable(
+        activity: Activity,
+        onShowAdCompleteListener: MainApplication.OnShowAdCompleteListener
+    ) {
+        // If the app open ad is already showing, do not show the ad again.
+        if (isShowingAd) {
+            Log.d(LOG_TAG, "The app open ad is already showing.")
+            return
+        }
+
+        // If the app open ad is not available yet, invoke the callback then load the ad.
+        if (!isAdAvailable()) {
+            Log.d(LOG_TAG, "The app open ad is not ready yet.")
+            onShowAdCompleteListener.onShowAdComplete()
+            loadAd(activity)
+            return
+        }
+
+        appOpenAd?.fullScreenContentCallback = object : FullScreenContentCallback() {
+
+            override fun onAdDismissedFullScreenContent() {
+                // Called when full screen content is dismissed.
+                // Set the reference to null so isAdAvailable() returns false.
+                Log.d(LOG_TAG, "Ad dismissed fullscreen content.")
+                appOpenAd = null
+                isShowingAd = false
+
+                onShowAdCompleteListener.onShowAdComplete()
+                loadAd(activity)
+            }
+
+            override fun onAdFailedToShowFullScreenContent(adError: AdError) {
+                // Called when fullscreen content failed to show.
+                // Set the reference to null so isAdAvailable() returns false.
+                Log.d(LOG_TAG, adError.message)
+                appOpenAd = null
+                isShowingAd = false
+
+                onShowAdCompleteListener.onShowAdComplete()
+                loadAd(activity)
+            }
+
+            override fun onAdShowedFullScreenContent() {
+                // Called when fullscreen content is shown.
+                Log.d(LOG_TAG, "Ad showed fullscreen content.")
+            }
+        }
+        isShowingAd = true
+        appOpenAd?.show(activity)
+    }
+
+    companion object {
+        private const val LOG_TAG = "AppOpenAdManager"
+        private const val AD_TEST_UNIT_ID = "ca-app-pub-3940256099942544/3419835294"
+        private const val AD_UNIT_ID = "ca-app-pub-8550386526282187/4965537905"
+    }
+}
--- app/src/main/java/com/gonnaggstudio/codescanner/di/AppModule.kt
+++ app/src/main/java/com/gonnaggstudio/codescanner/di/AppModule.kt
@@ -2,11 +2,13 @@ package com.gonnaggstudio.codescanner.di
 
 import android.content.Context
 import androidx.room.Room
+import com.gonnaggstudio.codescanner.ads.AppOpenAdManager
 import com.gonnaggstudio.codescanner.db.AppDatabase
 import com.gonnaggstudio.codescanner.db.Constants.BARCODE_DATABASE
 import com.gonnaggstudio.codescanner.db.dao.BarcodeDao
 import com.gonnaggstudio.codescanner.pref.DatastoreManager
 import com.gonnaggstudio.codescanner.utils.clipboard.ClipboardManagerHelper
+import com.google.android.gms.ads.MobileAds
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -43,5 +45,14 @@ class AppModule {
     @Provides
     fun provideDatastoreManager(@ApplicationContext appContext: Context): DatastoreManager {
         return DatastoreManager(appContext)
+    }
+
+    @Singleton
+    @Provides
+    fun provideAppOpenAdManager(@ApplicationContext appContext: Context): AppOpenAdManager {
+        MobileAds.initialize(
+            appContext
+        ) { }
+        return AppOpenAdManager()
     }
 }
